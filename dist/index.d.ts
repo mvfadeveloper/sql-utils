45d0cdd3accbb7959c9620d7c982ecca8a493331
Yasin Fouladi <fouladiyas@gmail.com>
2025-03-02 11:37:28 +0300

    fix: add d.ts

---
+++ dist/index.d.ts
@@ -0,0 +1,24 @@
+import { InputData, Result } from "./index.d";
+export declare class SqlUtils {
+    private sql;
+    constructor(connectionString: string);
+    getSingleObj<T>(table: string, queryParam: string, queryValue: string | number | boolean): Promise<Result<T, string>>;
+    getAllObjs<T>(table: string, options?: {
+        queryParam?: string;
+        queryValue?: string | number | boolean;
+    }): Promise<Result<T, string>>;
+    getAllObjsPaginated<T>(table: string, options?: {
+        queryParam?: string;
+        queryValue?: string | number | boolean;
+        page?: number;
+        pageSize?: number;
+    }): Promise<Result<{
+        data: T;
+        total: number;
+        pageCount: number;
+    }, string>>;
+    updateSingleColumn<T>(table: string, column: string, value: string | string[] | number | boolean | null, idQuery: number, idColumn?: string | undefined): Promise<Result<T, string>>;
+    updateData<T>(table: string, updateData: InputData, idQuery: number, idColumn?: string): Promise<Result<T, string>>;
+    deleteSingleRow(table: string, id: number): Promise<Result<void, string>>;
+    insertData<T>(table: string, insertData: InputData): Promise<Result<T, string>>;
+}
